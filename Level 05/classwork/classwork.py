132dafa130f273e6c869c266b44fd0b7530b54a9
sabatatishvili <sabatato2009@gmail.com>
2025-03-13 23:05:09 +0400

    second commit

---
+++ Level 05/classwork/classwork.py
@@ -0,0 +1,22 @@
+
+#for i in range(2, 26):
+#if i % 2 != 0:
+#print(i)
+#--------------------------
+#name = input("Enter your name: ")
+#for letter in name:
+#    print(letter)
+#----------------------------
+#correct_password = "my_secret_password"
+#user_password = input("Enter your password: ")
+#while user_password != correct_password:
+    #print("Incorrect password. Please try again.")
+    #user_password = input("Enter your password: ")
+#print("Access granted!")
+
+
+
+
+
+
+
